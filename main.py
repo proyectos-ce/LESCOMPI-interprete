64e73c0b6cf5bfa9d954834b6987883164f10af1
DanielC <daniel.esteban.8@hotmail.com>
2018-03-31 18:36:17 -0600

    Agrega la función percent_analyzer al SyntaxAnalyzer

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from analyzer import Analyzer
 from fake_sender import FakeSender
 from list_parser import ListParser
+from interface import Interface
 
 
 def main():
     global fs, analyzer
     fs = FakeSender()
     analyzer = Analyzer(fs.get_queue(), 11)
+    interface = Interface()
 
 #print(fs.get_queue().qsize())
 
