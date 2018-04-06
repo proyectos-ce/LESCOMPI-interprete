46d4e4acd64dcd12faf18f71f939e85ee12e43ac
DanielC <daniel.esteban.8@hotmail.com>
2018-04-06 17:32:01 -0600

    Agrega base interfaz

--- interface.py
+++ interface.py
@@ -3,34 +3,33 @@ import threading
 from tkinter import *
 
 
+
 class Interface:
     """
     Does something
     """
 
     def __init__(self):
-
         root = Tk()
-        frame = Frame(root)
-        frame.pack()
-
-        bottomframe = Frame(root)
-
-        bottomframe.pack(side=BOTTOM)
-
-        redbutton = Button(frame, text="Red", fg="red")
-        redbutton.pack(side=LEFT)
-
-        greenbutton = Button(frame, text="Brown", fg="brown")
-        greenbutton.pack(side=LEFT)
-
-        bluebutton = Button(frame, text="Blue", fg="blue")
-        bluebutton.pack(side=LEFT)
-
-        blackbutton = Button(bottomframe, text="Black", fg="black")
-        blackbutton.pack(side=BOTTOM)
-
+        root.geometry("1000x600")
+        canvas = Canvas(root, width=1000, height=600)
+        #canvas.grid(row=0, column=0)
+        root.title('Test')
+        photo = PhotoImage(file='bgrnd.gif')
+        canvas.create_image(480, 300, image=photo)
+        canvas.pack()
         root.title("Hola mundo")
         root.iconbitmap(r'icon.ico')
-
         root.mainloop()
+        #
+        # root = Tk()
+        # root.geometry("1000x600")
+        # frame = Frame(root)
+        # frame.pack()
+        # canvas = Canvas(frame)
+        # #canvas.grid(row=0, column=0)
+        # photo = PhotoImage(file='bgrnd.gif')
+        # canvas.create_image(0, 0, image=photo)
+        # root.title("Hola mundo")
+        # root.iconbitmap(r'icon.ico')
+        # root.mainloop()
